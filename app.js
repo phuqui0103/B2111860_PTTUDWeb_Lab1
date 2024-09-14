0c64fcd58a38467bb062715a3a35070bfa606335
Vo Phu Qui <145702431+phuqui0103@users.noreply.github.com>
2024-09-15 02:38:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line no-undef
+const contactsRouter = require("./app/routes/contact.route");
+// eslint-disable-next-line no-undef
 const express = require("express");
 // eslint-disable-next-line no-undef
 const cors = require("cors");
@@ -11,6 +13,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
+app.use("/api/contacts", contactsRouter);
 
 // eslint-disable-next-line no-undef
 module.exports = app;
+
